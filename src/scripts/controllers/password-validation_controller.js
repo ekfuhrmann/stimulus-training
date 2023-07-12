8dd5b427d0551f748d859ff533f4e5cdea0663ef
Eric <ekfuhrmann@gmail.com>
2023-07-12 12:43:45 -0600

    added some regex comments

---
+++ src/scripts/controllers/password-validation_controller.js
@@ -0,0 +1,11 @@
+// regex helpers
+
+/**
+ * REGEX Helpers
+ *
+ * check length
+ * check uppercase: /[A-Z]/
+ * check number: /\d/
+ * check special character: /[!"#$%&'()*+,-.\\/:;<=>?@[\]^_`{|}~]/
+ * check fields match
+ */
